1cd09bf88443a42592556f4caa982ca8d9e9ae77
Sam33rRanjan <sameerranjan2003@gmail.com>
2025-01-24 21:30:40 +0530

    Update Progress

---
+++ 046_JS_TemporalDeadZone.js
@@ -0,0 +1,4 @@
+console.log(marks);
+console.log("Hello");
+console.log("World");
+let marks=100; // Line 1 to Line 4 is the Temporal Dead Zone i.e. we can't access the variable before it is declared.
